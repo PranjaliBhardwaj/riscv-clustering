3c6f7f6ae633bf516204de12ec5082ed8b3a64c1
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2025-05-05 14:23:32 +0530

    Create riscv_utils.h

---
+++ include/riscv_utils.h
@@ -0,0 +1,16 @@
+#ifndef RISCV_UTILS_H
+#define RISCV_UTILS_H
+
+#include <stdint.h>
+
+// Vector distance calculation - ASM optimized
+double riscv_euclidean_distance(const double* a, const double* b, uint32_t dim);
+
+void riscv_matrix_mean(double* output, const double* input, 
+                      uint32_t rows, uint32_t cols, uint32_t stride);
+
+// Performance monitoring
+void riscv_start_counters();
+void riscv_stop_counters();
+
+#endif
